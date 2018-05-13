63407e0706ad99facac84f1936639edbdc8cc2bc
Tyler <tpitsch@gmail.com>
2018-05-13 11:32:56 -0700

    inital

---
+++ src/GUI/MainWindow.java
@@ -0,0 +1,44 @@
+package GUI;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class MainWindow extends Application {
+    
+	private Stage primaryStage;
+	
+    @Override
+    public void start(Stage primaryStage) {
+      this.primaryStage = primaryStage;
+      mainWindow();
+    }
+    
+    public void mainWindow() {
+    	try {
+    		FXMLLoader loader = new FXMLLoader(MainWindow.class.getResource("/theWindow.fxml"));
+    		AnchorPane pane = loader.load();
+    	
+    		Scene scene = new Scene(pane);
+    		
+    		WindowControl control = loader.getController();
+    		
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    
+    }
+    
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
